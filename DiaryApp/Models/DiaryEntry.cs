c7abd75ce8810ff9f9cc8bcdcda5571959777b04
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-12-21 17:41:04 +1100

    Diary Entry model added

---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+// key is the unique identifier about the model you are talking about
+
+        //[Key] if name not Id and something like DiaryId
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty ;
+
+        [Required]
+        public DateTime Created {  get; set; } = DateTime.Now ;
+
+
+    }
+}
