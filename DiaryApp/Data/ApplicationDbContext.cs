b15cec73d88a3cd64278b3320d0b432818516190
Tanisha Sharma <tanishaofficial30@gmail.com>
2025-12-23 07:30:50 +1100

    diary entries added to frontend

--- DiaryApp/Data/ApplicationDbContext.cs
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DiaryApp.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DiaryApp.Data
 {
@@ -10,15 +11,46 @@ namespace DiaryApp.Data
 
 
         }
-
-        public DbSet<DiaryEntry> DiaryEntries { get; set; }
-
-
         // Four Steps to Add aTable
         // 1. Create A Model Class
         // 2. Add DB Set
         // 3. add-migration AddDiaryEntryTable
         // 4. update-database
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        //override this method to seed data to our database
+
+        //Tools -> Nuget Manager Console -> add-migration AddedSeedingDataDiaryEntry (basically update)
+        //update-database (to actually update the database)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // HasData and HasKey to remember
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry 
+                { 
+                    Id = 1, Title = "Went hiking", Content = "Went hiking with Joe!", Created = DateTime.Now 
+                },
+                new DiaryEntry { 
+                    Id = 2, Title = "Made Tea", Content = "Made Tea with Angela!", Created = DateTime.Now 
+                },
+                new DiaryEntry { 
+                    Id = 3, Title = "Went diving", Content = "Went diving with Christie!", Created = DateTime.Now 
+                }
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings =>
+                warnings.Ignore(RelationalEventId.PendingModelChangesWarning)
+            );
+        }
+
+
+
 
     }
 }
---
+++ DiaryApp/Migrations/20251222173557_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2025, 12, 23, 4, 35, 57, 467, DateTimeKind.Local).AddTicks(2323), "Went hiking" },
+                    { 2, "Made Tea with Angela!", new DateTime(2025, 12, 23, 4, 35, 57, 467, DateTimeKind.Local).AddTicks(2499), "Made Tea" },
+                    { 3, "Went diving with Christie!", new DateTime(2025, 12, 23, 4, 35, 57, 467, DateTimeKind.Local).AddTicks(2503), "Went diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
